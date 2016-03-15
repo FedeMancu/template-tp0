de64b43ac0d68cfdeb437d9d5efd43d0eab113f3
FedeMancu <mancusofe@gmail.com>
2016-03-14 23:32:36 -0300

    Primeros avances en la implementacion.

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -2,22 +2,25 @@ package ar.fiuba.tdd.template.tp0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
+
     //private int maxLength;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    public RegExGenerator(/*int maxLength*/) {
+        //this.maxLength = maxLength;
+    }
 
-    // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
+    public List<String> generate(String regEx, int numberOfResults) {
         return new ArrayList<String>() {
             {
-                add("a");
-                add("b");
-                add("c");
+                for (int i=0; i< numberOfResults ; i++) {
+                    StringTokenizer multiTokenizer = new StringTokenizer(regEx, "?*+");
+                    while (multiTokenizer.hasMoreTokens()) {
+                        add(multiTokenizer.nextToken());
+                    }
+                }
             }
         };
     }
